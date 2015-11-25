867e50e3dc9db601e92798a69abb1d5b2693bc44
nevillco <nevillco@bc.edu>
2015-11-25 12:08:55 -0500

    Initial Commit

---
+++ pSylow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pSylow
+//
+//  Created by Connor Neville on 11/25/15.
+//  Copyright © 2015 connorneville. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pSylow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pSylow
+//
+//  Created by Connor Neville on 11/25/15.
+//  Copyright © 2015 connorneville. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
